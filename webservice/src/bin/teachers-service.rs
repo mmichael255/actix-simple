0f5b1f24d62aab5afba7fa32391dba3e5d345449
mmichael255 <cby371@gmail.com>
2024-07-21 17:37:52 +0900

    post

--- webservice/src/bin/teachers-service.rs
+++ webservice/src/bin/teachers-service.rs
@@ -11,6 +11,9 @@ mod routers;
 #[path = "../state.rs"]
 mod state;
 
+#[path = "../models.rs"]
+mod models;
+
 use routers::*;
 use state::AppState;
 #[actix_rt::main]
@@ -18,12 +21,14 @@ async fn main() -> io::Result<()> {
     let shared_data = web::Data::new(AppState {
         health_check_response: "I am Ok".to_string(),
         visit_count: Mutex::new(0),
+        courses: Mutex::new(vec![]),
     });
 
     let app = move || {
         App::new()
             .app_data(shared_data.clone())
             .configure(general_routes)
+            .configure(course_routes)
     };
     HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
 }
--- webservice/src/handlers.rs
+++ webservice/src/handlers.rs
@@ -1,5 +1,31 @@
+use super::models::Course;
 use super::state::AppState;
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
+
+pub async fn new_course(
+    new_course: web::Json<Course>,
+    app_state: web::Data<AppState>,
+) -> HttpResponse {
+    println!("Received new course");
+    let course_count = app_state
+        .courses
+        .lock()
+        .unwrap()
+        .clone()
+        .into_iter()
+        .filter(|course| course.teacher_id == new_course.teacher_id)
+        .collect::<Vec<Course>>()
+        .len();
+    let new_course = Course {
+        teacher_id: new_course.teacher_id,
+        id: Some(course_count + 1),
+        name: new_course.name.clone(),
+        time: Some(Utc::now().naive_utc()),
+    };
+    app_state.courses.lock().unwrap().push(new_course);
+    HttpResponse::Ok().json("Course added")
+}
 
 pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpResponse {
     let health_check_response = &app_state.health_check_response;
@@ -7,4 +33,30 @@ pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpRespons
     let response = format!("{} {} times", health_check_response, visit_count);
     *visit_count += 1;
     HttpResponse::Ok().json(&response)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::http::StatusCode;
+    use std::sync::Mutex;
+
+    #[actix_rt::test]
+    async fn post_test() {
+        let course = web::Json(Course {
+            teacher_id: 1,
+            name: "Test Course".into(),
+            id: None,
+            time: None,
+        });
+        let app_state = web::Data::new(AppState {
+            health_check_response: "".into(),
+            courses: Mutex::new(vec![]),
+            visit_count: Mutex::new(0),
+        });
+
+        let resp = new_course(course, app_state).await;
+
+        assert_eq!(resp.status(), StatusCode::OK)
+    }
 }
